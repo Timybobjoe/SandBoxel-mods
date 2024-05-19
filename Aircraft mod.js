1079f2992a983c95c6718fc72e5e6ad067f10fe3
Timybobjoe <159643711+Timybobjoe@users.noreply.github.com>
2024-05-19 19:59:10 -0400

    Update Aircraft mod.js

--- Aircraft mod.js
+++ Aircraft mod.js
@@ -124,34 +124,44 @@ elements.fighter_jet_left = {
         // Check for walls on the left side
         var wallOnLeft = !isEmpty(pixel.x - 1, pixel.y, true);
 
-        for (var i = 0; i < 2; i++) {
-            if (wallOnLeft) {
-                // If there's a wall on the left, move right
-                tryMove(pixel, pixel.x + 1, pixel.y);
-            } else {
-                // Otherwise, move left
-                tryMove(pixel, pixel.x - 1, pixel.y);
-            }
-
-            // Check for collision after movement
-            if (!tryMove(pixel, pixel.x, pixel.y)) {
-                var newPixel = pixelMap[pixel.x][pixel.y];
-                if (newPixel.element === "fast_bullet_left") { 
-                    break; 
-                }
-                if (elements[newPixel.element].state == "solid") {
-                    if (Math.random() > (elements[newPixel.element].hardness || 0)) {
-                        if (elements[newPixel.element].breakInto) {
-                            breakPixel(newPixel);
-                        } else {
-                            deletePixel(newPixel.x, newPixel.y);
-                        }
-                    }
-                }
-                // Delete the current pixel if collision
-                deletePixel(pixel.x, pixel.y);
+        // Calculate the distance from the wall
+        var distanceFromWall = 0;
+        for (var i = 1; i <= 15; i++) {
+            if (!isEmpty(pixel.x - i, pixel.y, true)) {
+                distanceFromWall = i;
                 break;
             }
+        }
+
+        // If the jet is less than 15 pixels away from the wall, turn around
+        if (distanceFromWall <= 15) {
+            wallOnLeft = !wallOnLeft; // Reverse direction
+        }
+
+        // Move according to the direction
+        if (wallOnLeft) {
+            tryMove(pixel, pixel.x - 1, pixel.y); // Move left
+        } else {
+            tryMove(pixel, pixel.x + 1, pixel.y); // Move right
+        }
+
+        // Check for collision after movement
+        if (!tryMove(pixel, pixel.x, pixel.y)) {
+            var newPixel = pixelMap[pixel.x][pixel.y];
+            if (newPixel.element === "fast_bullet_left") {
+                break;
+            }
+            if (elements[newPixel.element].state == "solid") {
+                if (Math.random() > (elements[newPixel.element].hardness || 0)) {
+                    if (elements[newPixel.element].breakInto) {
+                        breakPixel(newPixel);
+                    } else {
+                        deletePixel(newPixel.x, newPixel.y);
+                    }
+                }
+            }
+            // Delete the current pixel if collision
+            deletePixel(pixel.x, pixel.y);
         }
     },
     category: "aircrafts",
